ba17a93a0af6fc606e03c3688528283285f5e427
mrkarmadip009 <alpanena94994@gmail.com>
2026-02-20 03:14:11 +0530

    i changed some file and solved 403 error

--- readloom/urls.py
+++ readloom/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
+    path('', home, name='home'),   
     path('admin/', admin.site.urls),
     path('authentication/', include("authentication.urls")),
-
 ]
